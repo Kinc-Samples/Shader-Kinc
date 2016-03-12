3500b80184b536e5b436167f24a21ee9373852ad
Robert Konrad <robert@ktx-software.com>
2016-03-12 03:56:27 +0100

    Kore update.

--- Sources/Shader.cpp
+++ Sources/Shader.cpp
@@ -1,9 +1,9 @@
 #include "pch.h"
 
-#include <Kore/Application.h>
 #include <Kore/IO/FileReader.h>
 #include <Kore/Graphics/Graphics.h>
 #include <Kore/Graphics/Shader.h>
+#include <Kore/System.h>
 #include <limits>
 
 using namespace Kore;
@@ -30,8 +30,22 @@ namespace {
 }
 
 int kore(int argc, char** argv) {
-	Application* app = new Application(argc, argv, 1024, 768, 0, false, "ShaderTest");
-	app->setCallback(update);
+    Kore::System::setName("Shader");
+	Kore::System::setup();
+	Kore::WindowOptions options;
+	options.title = "Shader";
+	options.width = 1024;
+	options.height = 768;
+	options.x = 100;
+	options.y = 100;
+	options.targetDisplay = 0;
+	options.mode = WindowMode::Window;
+	options.rendererOptions.depthBufferBits = 16;
+	options.rendererOptions.stencilBufferBits = 8;
+	options.rendererOptions.textureFormat = 0;
+	options.rendererOptions.antialiasing = 0;
+	Kore::System::initWindow(options);
+	Kore::System::setCallback(update);
 
 	FileReader vs("shader.vert");
 	FileReader fs("shader.frag");
@@ -43,20 +57,20 @@ int kore(int argc, char** argv) {
 	program->setVertexShader(vertexShader);
 	program->setFragmentShader(fragmentShader);
 	program->link(structure);
-	
+
 	vertices = new VertexBuffer(3, structure);
 	float* v = vertices->lock();
 	v[0] = -1; v[1] = -1; v[2] = 0.5;
 	v[3] = 1;  v[4] = -1; v[5] = 0.5;
 	v[6] = -1; v[7] = 1;  v[8] = 0.5;
 	vertices->unlock();
-	
+
 	indices = new IndexBuffer(3);
 	int* i = indices->lock();
 	i[0] = 0; i[1] = 1; i[2] = 2;
 	indices->unlock();
 
-	app->start();
+	Kore::System::start();
 
 	return 0;
 }
