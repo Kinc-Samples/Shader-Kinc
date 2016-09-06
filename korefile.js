fb980b67845e417d638524825e0d7eb5118c7d92
Robert Konrad <git2016@robdangero.us>
2016-09-07 00:40:06 +0200

    Update Kore and korefile

--- korefile.js
+++ korefile.js
@@ -1,11 +1,9 @@
-var solution = new Solution('ShaderTest');
-var project = new Project('ShaderTest');
+let project = new Project('ShaderTest', __dirname);
 
 project.addFile('Sources/**');
 project.setDebugDir('Deployment');
 
-project.addSubProject(Solution.createProject('Kore'));
-
-solution.addProject(project);
-
-return solution;
+Project.createProject('Kore', __dirname).then((kore) => {
+	project.addSubProject(kore);
+	resolve(project);
+});
