0504cbf930ffe97d477561b020b79377ddcd2e19
Robert Konrad <git2016@robdangero.us>
2021-05-13 16:12:29 +0200

    Update Kinc

--- kincfile.js
+++ kincfile.js
@@ -1,6 +1,7 @@
-let project = new Project('ShaderTest');
+const project = new Project('ShaderTest');
 
 project.addFile('Sources/**');
+project.addFile('Shaders/**');
 project.setDebugDir('Deployment');
 
 resolve(project);
